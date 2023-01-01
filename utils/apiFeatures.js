0d96ff989b89565befb93a0a59a261d910871fd2
“himanshu.rai” <himanshu.rai@appinventiv.com>
2023-01-02 03:11:25 +0530

    forworking of code degrade the moongoose ver 6 to 5 due to mongoose v6 does not allow duplicate queries

--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -1,63 +1,62 @@
 class APIFeatures {
-    constructor(query, queryStr) {
-        this.query = query;
-        this.queryStr = queryStr;
-    }
+  constructor(query, queryStr) {
+    this.query = query;
+    this.queryStr = queryStr;
+  }
 
-    search() {
-        const location = this.queryStr.location ? {
-            address: {
-                $regex: this.queryStr.location,
-                $options: 'i'
-            }
-        } : {}
+  search() {
+    const location = this.queryStr.location
+      ? {
+          address: {
+            $regex: this.queryStr.location,
+            $options: 'i',
+          },
+        }
+      : {};
 
-        this.query = this.query.find({ ...location })
-        return this;
-    }
+    this.query = this.query.find({ ...location });
+    return this;
+  }
 
+  filter() {
+    const queryCopy = { ...this.queryStr };
 
-    filter() {
+    // Remove fields from query
+    const removeFields = ['location', 'page'];
+    removeFields.forEach((el) => delete queryCopy[el]);
 
-        const queryCopy = { ...this.queryStr }
+    // Advance filtering with gt, gte etc
+    // let filterQuery = {};
+    // const fieldsLength = Object.keys(queryCopy).length;
 
-        // Remove fields from query
-        const removeFields = ['location', 'page']
-        removeFields.forEach(el => delete queryCopy[el]);
+    // for (let i = 0; i < fieldsLength; i++) {
 
+    //     let queryStr = JSON.stringify(Object.keys(queryCopy)[i])
+    //     const filterField = queryStr.substring(1, queryStr.indexOf('['))
 
-        // Advance filtering with gt, gte etc
-        // let filterQuery = {};
-        // const fieldsLength = Object.keys(queryCopy).length;
+    //     if (filterField.length > 1) {
+    //         const fieldValue = Object.values(queryCopy)[0]
 
-        // for (let i = 0; i < fieldsLength; i++) {
+    //         const filterOperator = queryStr.substring(queryStr.indexOf('[') + 1, queryStr.indexOf(']'))
 
-        //     let queryStr = JSON.stringify(Object.keys(queryCopy)[i])
-        //     const filterField = queryStr.substring(1, queryStr.indexOf('['))
+    //         filterQuery = { ...filterQuery, [filterField]: { [`$${filterOperator}`]: fieldValue } }
 
-        //     if (filterField.length > 1) {
-        //         const fieldValue = Object.values(queryCopy)[0]
+    //     } else {
+    //         filterQuery = { ...filterQuery, [Object.keys(queryCopy)[i]]: Object.values(queryCopy)[i] }
+    //     }
 
-        //         const filterOperator = queryStr.substring(queryStr.indexOf('[') + 1, queryStr.indexOf(']'))
+    // }
 
-        //         filterQuery = { ...filterQuery, [filterField]: { [`$${filterOperator}`]: fieldValue } }
+    this.query = this.query.find(queryCopy);
+    return this;
+  }
+  pagination(resPerPage) {
+    const currentPage = Number(this.queryStr.page) || 1;
+    const skip = resPerPage * (currentPage - 1);
 
-        //     } else {
-        //         filterQuery = { ...filterQuery, [Object.keys(queryCopy)[i]]: Object.values(queryCopy)[i] }
-        //     }
-
-        // }
-
-        this.query = this.query.find(queryCopy);
-        return this;
-    }
-    pagination(resPerPage) {
-        const currentPage = Number(this.queryStr.page) || 1;
-        const skip = resPerPage * (currentPage - 1);
-
-        this.query = this.query.limit(resPerPage).skip(skip);
-        return this;
-    }
+    this.query = this.query.limit(resPerPage).skip(skip);
+    return this;
+  }
 }
 
 export default APIFeatures;
